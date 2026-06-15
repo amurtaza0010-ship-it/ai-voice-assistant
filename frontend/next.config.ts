aeba8ee26105f4522a0487adf1fe3e9aa81e0dc3
Ali Murtaza <amrtaza.0010@gmail.com>
2026-06-15 20:45:19 +0500

    my latest changes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   output: "standalone",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
